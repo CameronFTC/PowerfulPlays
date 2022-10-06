04a99d9d2cccaad9bafcc7d9a68c8726f7dec61c
CameronFTC <56981386+CameronFTC@users.noreply.github.com>
2022-10-06 13:05:58 -0500

    integrated trig mecanum with auto aim fortnite funny sex

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TFod.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TFod.java
@@ -126,13 +126,7 @@ public class TFod extends LinearOpMode {
                         if (updatedRecognitions.size() == 0){
                             error = 0;
                         }
-
-                        hw.bL.setPower(gamepad1.left_stick_y + error);
-                        hw.bR.setPower((-gamepad1.left_stick_y) + error);
-                        hw.fL.setPower(gamepad1.left_stick_y + error);
-                        hw.fR.setPower((-gamepad1.left_stick_y) + error);
-
-
+                        trigMecanum(error);
                         telemetry.update();
                     }
                 }
@@ -198,5 +192,43 @@ public class TFod extends LinearOpMode {
             heading *= -1;
 
         return -heading;
+    }
+
+    public void trigMecanum(double error) {
+
+        double rightstickx = Math.abs(gamepad1.right_stick_x) * -gamepad1.right_stick_x ;
+        double leftstickx = -gamepad1.left_stick_x * Math.abs(gamepad1.left_stick_x);
+
+        double leftstickyfront = Math.abs(gamepad1.left_stick_y) * gamepad1.left_stick_y ;
+        double leftstickyback = Math.abs(gamepad1.left_stick_y) * -gamepad1.left_stick_y ;
+
+        double rFront = Math.hypot(rightstickx, leftstickyfront);
+        double rBack = Math.hypot(rightstickx, leftstickyback);
+
+        double robotAngleFront = Math.atan2(leftstickyfront, rightstickx) - Math.PI / 4;
+        double robotAngleBack = Math.atan2(leftstickyback, rightstickx) - Math.PI / 4;
+
+        double rightX = leftstickx;
+
+        final double v1 = rFront * Math.cos(robotAngleFront) + rightX;
+        final double v2 = rFront * Math.sin(robotAngleFront) - rightX;
+        final double v3 = rBack * Math.sin(robotAngleBack) + rightX;
+        final double v4 = rBack * Math.cos(robotAngleBack) - rightX;
+
+        /*
+        telemetry.addData("fl", v1);
+        telemetry.addData ("fR", v2);
+        telemetry.addData ("bL", v3);
+        telemetry.addData ("bR", v4);
+        telemetry.addData("leftX", gamepad1.left_stick_x);
+        telemetry.addData("leftY", gamepad1.left_stick_y);
+        telemetry.addData ("Right X", rightX);
+        telemetry.update();
+        */
+
+        hw.fL.setPower(v1 + error);
+        hw.fR.setPower(-v2 + error);
+        hw.bL.setPower(-v3 + error);// * .79);
+        hw.bR.setPower(v4+ error);// * .79);
     }
 }
