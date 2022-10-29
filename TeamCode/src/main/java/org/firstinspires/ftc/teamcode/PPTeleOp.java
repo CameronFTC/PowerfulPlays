0e091b5605d299c13470c73221a4f92d4ea8f3c1
CameronFTC <56981386+CameronFTC@users.noreply.github.com>
2022-10-29 17:11:45 -0500

    changed direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PPTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PPTeleOp.java
@@ -1,8 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp(group = "TeleOp", name = "PPTeleOp")
 public class PPTeleOp extends LinearOpMode {
@@ -11,16 +18,49 @@ public class PPTeleOp extends LinearOpMode {
     public DcMotor bR;
     public DcMotor bL;
 
+    public BNO055IMU imu;
+    public Orientation lastAngles = new Orientation();
+    public Orientation startPos = null;
+    public double lastDegrees;
+    public double globalAngle;
+    public double referenceAngle;
+    private double currGyro;
+
     @Override
     public void runOpMode() {
         fL = hardwareMap.get(DcMotor.class, "fL");
         fR = hardwareMap.get(DcMotor.class, "fR");
         bL = hardwareMap.get(DcMotor.class, "bL");
         bR = hardwareMap.get(DcMotor.class, "bR");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+
+        imu = this.hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
         fL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bL.setDirection(DcMotorSimple.Direction.REVERSE);
+        fL.setDirection(DcMotorSimple.Direction.REVERSE);
+        this.telemetry.addData("Mode", "calibrating...");
+        this.telemetry.update();
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!this.isStopRequested() && !imu.isGyroCalibrated()) {
+            this.sleep(50);
+            this.idle();
+        }
+
+        this.telemetry.addData("Mode", "waiting for start");
+        this.telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
+        this.telemetry.update();
+        currGyro =  getAngle();
         waitForStart();
         while(opModeIsActive()){
             trigMecanum();
@@ -54,7 +94,34 @@ public class PPTeleOp extends LinearOpMode {
         }
     }
 
+    public double getAngle() {
+        // We experimentally determined the Z axis is the axis we want to use for heading angle.
+        // We have to process the angle because the imu works in euler angles so the Z axis is
+        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngles = angles;
+
+        return -globalAngle;
+    }
+
     public void trigMecanum() {
+        double gyroCorrect = (getAngle() - currGyro) * 0.01;
+
+        if(Math.abs(gamepad1.right_stick_x) > 0){
+            currGyro = getAngle();
+        }
 
         double rightstickx = Math.abs(gamepad1.right_stick_x) * -gamepad1.right_stick_x ;
         double leftstickx = -gamepad1.left_stick_x * Math.abs(gamepad1.left_stick_x);
@@ -87,9 +154,9 @@ public class PPTeleOp extends LinearOpMode {
         */
 
         fL.setPower(-v1);
-        fR.setPower(-v2);
+        fR.setPower(-v2 + currGyro);
         bL.setPower(v3);// * .79);
-        bR.setPower(v4);// * .79);
+        bR.setPower(v4 + currGyro);// * .79);
 
         telemetry.addData("fl", -v1);
         telemetry.addData ("fR", -v2);
