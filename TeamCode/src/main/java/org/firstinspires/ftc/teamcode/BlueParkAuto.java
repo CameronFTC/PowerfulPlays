a74125c3fe782eed0efe501730c98fff80f75b95
anjaligorti <54911406+anjaligorti@users.noreply.github.com>
2022-11-08 15:24:01 -0600

    vision merging

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueParkAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueParkAuto.java
@@ -1,12 +1,52 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @Autonomous
 public class BlueParkAuto extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+          hwMap robot = new hwMap(this);
+          Vision vision = new Vision(this);
+
+          String parkDist = "red";
+
+          while (!(isStarted() || isStopRequested())) {
+
+              //SAMPLE HERE
+                parkDist = vision.sample();
+
+                telemetry.addData("Position", parkDist);
+                telemetry.addData("Position", "Inside of Left Wheel on right side of tile line");
+
+                telemetry.update();
+
+                idle();
+            }
+
+            int position = 2400;
+
+            switch (parkDist) {
+                case "red":
+                    position = 570;
+                    break;
+                case "green":
+                    position = 1420;
+                    break;
+                case "blue":
+                    position = 2470;
+                    break;
+            }
+
+        }
 
     }
-}
